177782e791017476aca328ee6c83cb064ebd0721
simekani <simekani.mabambe@gmail.com>
2021-01-19 17:46:53 +0200

    intro

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDLPL51zj229YW2n0S3dwBS2Qeo6pZspI0",
+    authDomain: "clone-dee3d.firebaseapp.com",
+    projectId: "clone-dee3d",
+    storageBucket: "clone-dee3d.appspot.com",
+    messagingSenderId: "811964141919",
+    appId: "1:811964141919:web:e9e28cef85d95c59a2a6ea",
+    measurementId: "G-1SM3FKL9LP"
+  };
